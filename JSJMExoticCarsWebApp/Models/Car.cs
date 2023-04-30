23c8b9a3fc8d223a739a59183920b1803edc9a39
Hugzxhugz <jameskenthagman@gmail.com>
2023-04-30 10:51:20 +0200

    Created car model as preparation to save car object to database.

---
+++ JSJMExoticCarsWebApp/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace JSJMExoticCarsWebApp.Models;
+
+public class Car
+{
+    public string carModel { get; set; }
+    public string carBrand { get; set; }
+    public int yearModel { get; set; }
+    public string description { get; set; }
+    public decimal price { get; set; }
+
+    public Car(string carModel, string carBrand, int yearModel, string description, decimal price)
+    {
+        this.carModel = carModel;
+        this.carBrand = carBrand;
+        this.yearModel = yearModel;
+        this.description = description;
+        this.price = price;
+    }
+   
+}
