9aff1bd575b4553f3f6c766573c008bfb80bd2b2
Hugzxhugz <jameskenthagman@gmail.com>
2023-05-01 11:30:24 +0200

    fixed aside toggle. used javascript to perform the toggle.

--- JSJMExoticCarsWebApp/wwwroot/js/site.js
+++ JSJMExoticCarsWebApp/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var aside = document.querySelector('aside');
+var toggleButton = document.querySelector('.toggle-aside');
+
+toggleButton.addEventListener('click', function() {
+    aside.classList.toggle('hidden')
+});
