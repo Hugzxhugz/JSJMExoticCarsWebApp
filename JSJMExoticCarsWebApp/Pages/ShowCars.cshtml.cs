813f159ab51b9f7c8b63453cb1663348342b0020
Hugzxhugz <jameskenthagman@gmail.com>
2023-05-03 14:10:46 +0200

    edited layouts. added showcars page.

---
+++ JSJMExoticCarsWebApp/Pages/ShowCars.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JSJMExoticCarsWebApp.Pages
+{
+    public class ShowCarsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
