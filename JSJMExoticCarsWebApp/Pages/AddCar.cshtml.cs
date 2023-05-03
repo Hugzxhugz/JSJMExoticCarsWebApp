93dab22225b5d63df383bd30548a3ce0b4cc25a3
Hugzxhugz <jameskenthagman@gmail.com>
2023-05-03 10:56:57 +0200

    edited for pointers in cars to sell forms.

--- JSJMExoticCarsWebApp/Pages/AddCar.cshtml.cs
+++ JSJMExoticCarsWebApp/Pages/AddCar.cshtml.cs
@@ -9,9 +9,6 @@ namespace JSJMExoticCarsWebApp.Pages;
 
 public class addCar : PageModel
 {
-    
-    private const string JsonFileName = "cars.json";
-    
     [BindProperty]
     public Car Car { get; set; }
     
@@ -19,33 +16,8 @@ public class addCar : PageModel
     {
     }
 
-    public IActionResult OnPost()
+    public void OnPost()
     {
-        if (ModelState.IsValid)
-        {
-            // Read the existing JSON file
-            var cars = new List<Car>();
-            if (System.IO.File.Exists(JsonFileName))
-            {
-                var json = System.IO.File.ReadAllText(JsonFileName);
-                cars = JsonSerializer.Deserialize<List<Car>>(json);
-            }
-
-            // Add the new car to the list
-            cars.Add(Car);
-
-            // Write the updated JSON file
-            var options = new JsonSerializerOptions
-            {
-                WriteIndented = true
-            };
-            var jsonString = JsonSerializer.Serialize(cars, options);
-            System.IO.File.WriteAllText(JsonFileName, jsonString);
-
-            // Redirect to the index page
-            return RedirectToPage("/Index");
-        }
-        return Page();
     }
 }
 
