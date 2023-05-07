c599af28878fa1b9180bb020d61bfb97ac982453
Hugzxhugz <jameskenthagman@gmail.com>
2023-05-07 22:31:07 +0200

    FIXED UPDATE AND DELETE

--- JSJMExoticCarsWebApp/Pages/EditCarDetails.cshtml.cs
+++ JSJMExoticCarsWebApp/Pages/EditCarDetails.cshtml.cs
@@ -39,32 +39,34 @@ public class EditCarDetailsModel : PageModel
         return Page();
     }
 
-    public IActionResult OnPost(int? id)
+    public IActionResult OnPost(int? id, string submitAction)
     {
-        if (id == null)
+        if (submitAction == "delete")
         {
-            return NotFound();
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carToDelete = dbc.Cars.Find(id);
+
+            if (carToDelete == null)
+            {
+                return NotFound();
+            }
+
+            // Set the Id property explicitly
+            carToDelete.Id = id.Value;
+
+            dbc.Cars.Remove(carToDelete);
+            dbc.SaveChanges();
+
+            TempData["Message"] = "Car deleted successfully.";
+
+            return RedirectToPage("/Market");
         }
 
-        var carToDelete = dbc.Cars.Find(id);
-
-        if (carToDelete == null)
-        {
-            return NotFound();
-        }
-
-        // Set the Id property explicitly
-        carToDelete.Id = id.Value;
-
-        dbc.Cars.Remove(carToDelete);
-        dbc.SaveChanges();
-
-        TempData["Message"] = "Car deleted successfully.";
-
-        return RedirectToPage("/Market");
-    }
-    public IActionResult OnPost(string submitAction)
-    {
         if (submitAction == "update")
         {
             if (!ModelState.IsValid)
@@ -72,11 +74,30 @@ public class EditCarDetailsModel : PageModel
                 return Page();
             }
 
-            dbc.Cars.Update(car);
+            var carToUpdate = dbc.Cars.Find(id);
+
+            if (carToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            carToUpdate.CarModel = car.CarModel;
+            carToUpdate.CarBrand = car.CarBrand;
+            carToUpdate.YearModel = car.YearModel;
+            carToUpdate.Description = car.Description;
+            carToUpdate.Mileage = car.Mileage;
+            carToUpdate.Transmission = car.Transmission;
+            carToUpdate.Fuel = car.Fuel;
+            carToUpdate.Price = car.Price;
+
+            dbc.Cars.Update(carToUpdate);
             dbc.SaveChanges();
+
             TempData["Message"] = "Car details updated successfully!";
+
             return RedirectToPage("/Market");
         }
+
         return Page();
     }
 }
