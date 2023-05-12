dcedf65f4c1082f2c88bc02719abb0fa89ed9019
babusch <115546515+babusch@users.noreply.github.com>
2023-05-12 18:37:32 +0200

    test

--- JSJMExoticCarsWebApp/Pages/Market.cshtml.cs
+++ JSJMExoticCarsWebApp/Pages/Market.cshtml.cs
@@ -6,18 +6,32 @@ namespace JSJMExoticCarsWebApp.Pages
 {
     public class MarketModel : PageModel
     {
-
+        public string ErrorMessage { get; set; }
         CarDbContext CarDbContext { get; set; }
         public MarketModel(CarDbContext cardb)
         {
             CarDbContext= cardb;
+            Cars = CarDbContext.Cars.ToList();
         }
 
         public List<Car> Cars { get; set; }
 
+
+
         public void OnGet()
         {
-            Cars = CarDbContext.Cars.ToList();
+            
+        }
+
+        public ActionResult OnPostAddCarPage()
+        {
+            if(HttpContext.Session.Get("UserSession") == null)
+            {
+                ErrorMessage = "You can't add a car if you are not signed in";
+                return Page();
+            }
+
+            return RedirectToPage("/AddCar");
         }
     }
 }
